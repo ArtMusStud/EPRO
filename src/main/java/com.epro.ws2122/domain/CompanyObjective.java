8a0ee93bba177112b111076f5ee3dec75fccd382
Artur <artur.mustaf@th-bingen.de>
2022-01-03 19:33:42 +0100

    test(controller): refine tests

--- src/main/java/com.epro.ws2122/domain/CompanyObjective.java
+++ src/main/java/com.epro.ws2122/domain/CompanyObjective.java
@@ -1,12 +1,19 @@
 package com.epro.ws2122.domain;
 
+import com.epro.ws2122.assembler.CompanyObjectiveAssembler;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Id;
 import javax.persistence.Entity;
 import java.util.Date;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class CompanyObjective {
 
--- src/test/java/com/epro/ws2122/CompanyObjectiveControllerTest.java
+++ src/test/java/com/epro/ws2122/CompanyObjectiveControllerTest.java
@@ -1,5 +1,6 @@
 package com.epro.ws2122;
 
+import com.epro.ws2122.controller.CompanyObjectiveController;
 import com.epro.ws2122.domain.CompanyObjective;
 import com.epro.ws2122.repository.CompanyObjectiveRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(CompanyObjectiveController.class)
 @AutoConfigureMockMvc
 class CompanyObjectiveControllerTest {
 
@@ -32,12 +33,20 @@ class CompanyObjectiveControllerTest {
 
     @BeforeEach
     public void initializeData() {
-        var companyObjective = new CompanyObjective();
-        companyObjective.setId(1L);
-        companyObjective.setName("Company Objective 1");
-        companyObjective.setOverall(0.75);
+        var companyObjective_0 = CompanyObjective.builder()
+                .id(1L)
+                .name("Company Objective 1")
+                .overall(0.75)
+                .build();
 
-        Mockito.when(mockRepository.findById(1L)).thenReturn(Optional.of(companyObjective));
+        var companyObjective_1 = CompanyObjective.builder()
+                .id(2L)
+                .name("Company Objective 2")
+                .overall(0.1)
+                .build();
+
+        Mockito.when(mockRepository.findById(1L)).thenReturn(Optional.of(companyObjective_0));
+        Mockito.when(mockRepository.findById(2L)).thenReturn(Optional.of(companyObjective_1));
     }
 
     @Test
