f9632384f99f29637c3cdf391457b29168416086
Artur <artur.mustaf@th-bingen.de>
2022-01-03 16:03:22 +0100

    fix(domain): add missing annotations

--- src/main/java/com.epro.ws2122/domain/CompanyObjective.java
+++ src/main/java/com.epro.ws2122/domain/CompanyObjective.java
@@ -2,14 +2,17 @@ package com.epro.ws2122.domain;
 
 import lombok.Data;
 
+import javax.persistence.Id;
+import javax.persistence.Entity;
 import java.util.Date;
 
 @Data
+@Entity
 public class CompanyObjective {
 
+    @Id
     private Long id;
     private String name;
     private Date createdAt = new Date();
     private double overall;
-
 }
