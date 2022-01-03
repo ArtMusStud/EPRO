235ebd46351ba2f0a8015136bb379d153836f00f
Artur <artur.mustaf@th-bingen.de>
2022-01-03 16:04:51 +0100

    test(!): test successful loading of spring application context

---
+++ src/test/java/com/epro/ws2122/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.epro.ws2122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
