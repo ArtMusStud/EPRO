85518a5dd508715769dca978d6dfb7f932cbfb5d
Artur <artur.mustaf@th-bingen.de>
2022-01-07 11:17:08 +0100

    feat(domain): add empty COKR entity and corresponding repository

---
+++ src/main/java/com.epro.ws2122/domain/CompanyObjectiveKeyResult.java
@@ -0,0 +1,20 @@
+package com.epro.ws2122.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CompanyObjectiveKeyResult {
+
+    @Id
+    private Long id;
+}
---
+++ src/main/java/com.epro.ws2122/repository/CompanyObjectiveKeyResultRepository.java
@@ -0,0 +1,7 @@
+package com.epro.ws2122.repository;
+
+import com.epro.ws2122.domain.CompanyObjectiveKeyResult;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyObjectiveKeyResultRepository extends CrudRepository<CompanyObjectiveKeyResult, Long> {
+}
