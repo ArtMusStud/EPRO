44c1f97d0ec743a6a514c9455726ce249ae5ecaa
Artur <artur.mustaf@th-bingen.de>
2022-01-07 10:28:23 +0100

    doc(ToDo): specify remaining todos

--- src/main/java/com.epro.ws2122/controller/CompanyObjectiveController.java
+++ src/main/java/com.epro.ws2122/controller/CompanyObjectiveController.java
@@ -7,7 +7,10 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -24,18 +27,26 @@ public class CompanyObjectiveController {
         this.assembler = assembler;
     }
 
-    //ToDo add remaining resources and links to response
+    /* ToDo add remaining resources and links to response:
+        collection resource of all CO KR corresponding to requested CO
+        link to complete aggregation at level of requested CO
+        link to dashboard
+     */
     @GetMapping( "/{id}")
     public ResponseEntity<RepresentationModel<CompanyObjectiveModel>> companyObjectiveById(@PathVariable("id") long id) {
         var companyObjective =  repository.findById(id);
         if (companyObjective.isPresent()) {
             var companyObjectiveResource = assembler.toModel(companyObjective.get());
+            companyObjectiveResource.add(
+                    linkTo(methodOn(CompanyObjectiveController.class).companyObjectives())
+                            .withRel("Company Objectives"));
             return new ResponseEntity<>(companyObjectiveResource, HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    //ToDo add remaining resources and links to response
+    /* ToDo add collection resource of all CO KR corresponding to each requested CO
+     */
     @GetMapping
     public ResponseEntity<CollectionModel<CompanyObjectiveModel>> companyObjectives() {
         var companyObjectives = repository.findAll();
