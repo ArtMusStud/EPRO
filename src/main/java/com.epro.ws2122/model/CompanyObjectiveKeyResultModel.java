7c6aa830528303aab315d5a3768b20df884f5742
Artur <artur.mustaf@th-bingen.de>
2022-01-07 11:38:14 +0100

    fix(hateoas): change generic type argument of model

--- src/main/java/com.epro.ws2122/model/CompanyObjectiveKeyResultModel.java
+++ src/main/java/com.epro.ws2122/model/CompanyObjectiveKeyResultModel.java
@@ -1,13 +1,9 @@
 package com.epro.ws2122.model;
 
-import com.epro.ws2122.domain.CompanyObjective;
 import com.epro.ws2122.domain.CompanyObjectiveKeyResult;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.hateoas.RepresentationModel;
 
-import java.util.Date;
-
-public class CompanyObjectiveKeyResultModel extends RepresentationModel<CompanyObjectiveModel> {
+public class CompanyObjectiveKeyResultModel extends RepresentationModel<CompanyObjectiveKeyResultModel> {
 
     public CompanyObjectiveKeyResultModel(CompanyObjectiveKeyResult companyObjectiveKeyResult) {
     }
