2526e014ef9246213c72f3883dfcbdccb9caa085
Artur <artur.mustaf@th-bingen.de>
2022-01-22 13:09:47 +0100

    fix(model): add missing properties to model of COK

--- src/main/java/com.epro.ws2122/model/CompanyKeyResultModel.java
+++ src/main/java/com.epro.ws2122/model/CompanyKeyResultModel.java
@@ -1,13 +1,25 @@
 package com.epro.ws2122.model;
 
 import com.epro.ws2122.domain.CompanyKeyResult;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
-/*
-    Todo: model needs properties of class CompanyObjectiveKeyResult
- */
+@Getter
+@Relation(collectionRelation = "companyKeyResults", value = "companyKeyResult")
 public class CompanyKeyResultModel extends RepresentationModel<CompanyKeyResultModel> {
 
+    private final String name;
+    private final double current;
+    private final double goal;
+    private final double confidence;
+
     public CompanyKeyResultModel(CompanyKeyResult companyObjectiveKeyResult) {
+        name = companyObjectiveKeyResult.getName();
+        current = companyObjectiveKeyResult.getCurrent();
+        goal = companyObjectiveKeyResult.getGoal();
+        confidence = companyObjectiveKeyResult.getConfidence();
     }
 }
