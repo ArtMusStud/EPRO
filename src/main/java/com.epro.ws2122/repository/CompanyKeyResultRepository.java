23e05f8f27fa617916ce13482b6924de62ccc1d5
Artur <artur.mustaf@th-bingen.de>
2022-02-02 11:22:59 +0100

    fix(bean management): add missing @repository annotation

--- src/main/java/com.epro.ws2122/repository/CompanyKeyResultRepository.java
+++ src/main/java/com.epro.ws2122/repository/CompanyKeyResultRepository.java
@@ -2,6 +2,8 @@ package com.epro.ws2122.repository;
 
 import com.epro.ws2122.domain.CompanyKeyResult;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyKeyResultRepository extends CrudRepository<CompanyKeyResult, Long> {
 }
