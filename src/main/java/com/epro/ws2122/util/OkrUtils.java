0e5b30552f2452625a52703b6f2810dba81124e8
Nikita Stephan <nikita.stephan@th-bingen.de>
2022-02-26 21:40:21 +0100

    doc(util): add javadocs to OkrUtils

--- src/main/java/com/epro/ws2122/util/OkrUtils.java
+++ src/main/java/com/epro/ws2122/util/OkrUtils.java
@@ -4,8 +4,16 @@ import com.epro.ws2122.domain.KeyResult;
 
 import java.util.List;
 
+/**
+ * <p>Utility class for calculations on entities and other things.</p>
+ */
 public class OkrUtils {
 
+    /**
+     * Calculates the overall progress of a list of {@link KeyResult}s
+     * @param keyResults keyresults
+     * @return overall progress
+     */
     public static double calculateOverall(List<? extends KeyResult> keyResults) {
         return keyResults.stream()
                 .mapToDouble( kr -> kr.getCurrent()/ kr.getGoal())
