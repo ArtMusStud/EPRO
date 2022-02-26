97cd5678ee2c371fec248ea737f18831006d1a0b
Nikita Stephan <nikita.stephan@th-bingen.de>
2022-02-26 22:15:34 +0100

    feat(domain): add owner to business unit key results

--- src/main/java/com/epro/ws2122/domain/BusinessUnitKeyResult.java
+++ src/main/java/com/epro/ws2122/domain/BusinessUnitKeyResult.java
@@ -1,5 +1,6 @@
 package com.epro.ws2122.domain;
 
+import com.epro.ws2122.domain.userRoles.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -12,6 +13,9 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 @Entity
 public class BusinessUnitKeyResult extends KeyResult {
+
+    @ManyToOne
+    private User owner;
 
     @ManyToOne
     private CompanyKeyResult companyKeyResult;
