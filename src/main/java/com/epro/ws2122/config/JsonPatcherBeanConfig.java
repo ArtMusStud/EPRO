a982d7f59b604d0822d7bd17dcb2b6d11361239a
Nikita Stephan <nikita.stephan@th-bingen.de>
2022-02-27 19:32:06 +0100

    feat(beans): add JsonPatcher beans for all okr entities

--- src/main/java/com/epro/ws2122/config/JsonPatcherBeanConfig.java
+++ src/main/java/com/epro/ws2122/config/JsonPatcherBeanConfig.java
@@ -1,6 +1,6 @@
 package com.epro.ws2122.config;
 
-import com.epro.ws2122.dto.KrUpdateDTO;
+import com.epro.ws2122.dto.*;
 import com.epro.ws2122.util.JsonPatcher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +15,25 @@ public class JsonPatcherBeanConfig {
     @Bean
     public JsonPatcher<KrUpdateDTO> krUpdatePatcher() {
         return new JsonPatcher<>(KrUpdateDTO.class);
+    }
+
+    @Bean
+    public JsonPatcher<CkrDTO> ckrPatcher() {
+        return new JsonPatcher<>(CkrDTO.class);
+    }
+
+    @Bean
+    public JsonPatcher<BukrDTO> bukrPatcher() {
+        return new JsonPatcher<>(BukrDTO.class);
+    }
+
+    @Bean
+    public JsonPatcher<CoDTO> coPatcher() {
+        return new JsonPatcher<>(CoDTO.class);
+    }
+
+    @Bean
+    public JsonPatcher<BuoDTO> buoPatcher() {
+        return new JsonPatcher<>(BuoDTO.class);
     }
 }
