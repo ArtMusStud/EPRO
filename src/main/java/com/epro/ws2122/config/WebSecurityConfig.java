09b5874fd2bc5e258983068d0017c1389b5c3853
Artur <artur.mustaf@th-bingen.de>
2022-02-28 17:22:23 +0100

    feat(security): add http basic with two users

--- src/main/java/com/epro/ws2122/config/WebSecurityConfig.java
+++ src/main/java/com/epro/ws2122/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.epro.ws2122.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,14 +15,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final String READ_ONLY_USER = "Read Only User";
     private final String CO_OKR_ADMIN = "CO OKR Admin";
     private final String BUO_OKR_ADMIN = "BUO OKR Admin";
-    
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}password")
+                .roles(READ_ONLY_USER);
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles(CO_OKR_ADMIN, BUO_OKR_ADMIN, READ_ONLY_USER);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         /* ToDo:
             - BUO OKR Admins dürfen nur ihre eigenen Resourcen bearbeiten
          */
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/company-objectives/**").hasAnyRole(CO_OKR_ADMIN, BUO_OKR_ADMIN, READ_ONLY_USER)
                 .antMatchers(HttpMethod.GET, "/company-objectives").hasAnyRole(CO_OKR_ADMIN, BUO_OKR_ADMIN, READ_ONLY_USER)
                 .antMatchers(HttpMethod.PATCH, "/company-objectives/**").hasRole(CO_OKR_ADMIN)
@@ -48,6 +65,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/business-unit-objectives/{buoId}/business-unit-key-results/**").hasRole(BUO_OKR_ADMIN)
                 .antMatchers(HttpMethod.POST, "/business-unit-objectives/{buoId}/business-unit-key-results").hasRole(BUO_OKR_ADMIN)
                 .antMatchers(HttpMethod.POST, "/business-unit-objectives/{buoId}/business-unit-key-results/{id}/link-with-company-key-result").hasRole(BUO_OKR_ADMIN)
-                .antMatchers(HttpMethod.DELETE, "/business-unit-objectives/{buoId}/business-unit-key-results/**").hasRole(BUO_OKR_ADMIN);
+                .antMatchers(HttpMethod.DELETE, "/business-unit-objectives/{buoId}/business-unit-key-results/**").hasRole(BUO_OKR_ADMIN)
+
+                .and().httpBasic();
     }
 }
