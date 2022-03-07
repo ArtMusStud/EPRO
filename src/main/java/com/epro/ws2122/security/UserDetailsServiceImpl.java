09eed8e9e366c92c7177deb0d99ef3d45ce0b15d
CyrosX <15161462+CyrosX@users.noreply.github.com>
2022-03-07 14:38:31 +0100

    feature(security): implement UserDetailsServiceImpl, UserPrincipal

---
+++ src/main/java/com/epro/ws2122/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.epro.ws2122.security;
+
+import com.epro.ws2122.domain.userRoles.User;
+import com.epro.ws2122.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null) throw new UsernameNotFoundException("User does not exist");
+        return new UserPrincipal(user);
+    }
+}
