11b6ff4d3efeb9c3fcf132db05cd3490cb043c14
Nikita Stephan <nikita.stephan@th-bingen.de>
2022-03-09 02:11:21 +0100

    feat(domain): add constraints and adjust equals and hashcode for user and role

--- src/main/java/com/epro/ws2122/domain/userRoles/Role.java
+++ src/main/java/com/epro/ws2122/domain/userRoles/Role.java
@@ -1,19 +1,20 @@
 package com.epro.ws2122.domain.userRoles;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private long id;
 
+    @Column(unique = true)
     private String name;
 }
